6086e8fb66db7613f4a8290fc660241ec9505988
Marcamillian <marcamillian@gmail.com>
2018-11-23 16:37:58 -0500

    test: GameScript chapter changing tests

--- UnitTests/t_GameScript.js
+++ UnitTests/t_GameScript.js
@@ -37,7 +37,7 @@ test("Testing getters", (t)=>{
     t.equals(testScript.getChapter("chapter2"), scriptData['chapter2'], "getChapterByName working correctly");
 
     t.throws(()=>{testScript.getChapter('something unknown'), /Named chapter not found/i, "Throw errror on none found"})
-    
+
     // get current chapter name
     t.equals(testScript.getChapterName(), "chapter1", "");
 
@@ -45,18 +45,27 @@ test("Testing getters", (t)=>{
     t.end()
 })
 
-test("Testing chapter checked", (t)=>{
+test("Testing chapter present in GameScript", (t)=>{
     let testScript = GameScript(scriptData);
 
     t.equals(testScript.hasChapter(Object.keys(scriptData)[1]), true, "Chapter correctly identified");
     t.equals(testScript.hasChapter("Wrong Chapter"), false, "Doesn't have the chapter")
+    t.equals(testScript.hasChapter(), false, "No chapter given to check") // ?? should this return an error or false // funfunfunction forum question
 
     t.end()
 })
 
 test.skip("Testing chapter changing", (t)=>{
     t.test("Chapter switching", (ts)=>{
+        let testScript = GameScript(scriptData);
+        let chapterNames = Object.keys(scriptData);
 
+        t.equals(testScript.nextChapter(), scriptData[chapterNames[1]], "Next chapter provided on chapter change")
+        t.throws(()=>{testScript.nextChapter()}, /End of chapters/i, "Can't go further than last chapter") 
+        t.equals(testScript.prevChapter(), scriptData[chapterNames[0]], "Previous chapter is correct")
+        t.throws(()=>{testScript.prevChapter()}, /Start of chapters/i, "Can't go before first chapter")
+        
+        t.end()
     })
 
     t.test("Page switching", (ts)=>{
